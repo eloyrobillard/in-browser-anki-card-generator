59f9ef82579fc5629338c89aac47242e14fb4ac8
Eloy Robillard <eloy.robillard@gmail.com>
2024-03-16 08:20:11 +0100

    Display popup button at start of selection

--- js/content-script.js
+++ js/content-script.js
@@ -2,22 +2,49 @@
  * @param {(...args: any) => void} f
  */
 function debounce(f, delay = 500) {
+	/** @type {number} */
 	let timeout;
-	return (...args) => {
+	return (/** @type {any[]} */ ...args) => {
 		clearTimeout(timeout);
-		timeout = setTimeout(() => f.apply(this, args), delay);
+		timeout = setTimeout(() => f(args), delay);
 	}
+}
+
+/**
+ * @param {number} top
+ * @param {number} left
+ */
+function createIframe(top, left) {
+	const iframe = document.createElement('iframe');
+
+	iframe.id = 'anki-generator-frame';
+	iframe.style.top = `${String(top)}px`;
+	iframe.style.left = `${String(left)}px`;
+	iframe.style.position = 'absolute';
+	iframe.style.width = '50px';
+	iframe.style.height = '50px';
+
+	const url = chrome.runtime.getURL('/button.html');
+	iframe.setAttribute('src', url);
+
+	return iframe;
 }
 
 document.addEventListener('selectionchange', debounce(() => {
 	const selection = document.getSelection();
-	if (selection) {
-		if (selection) {
-			const rect = selection.getRangeAt(0).getBoundingClientRect();
-			chrome.runtime.sendMessage({ message: 'create popup', data: rect }, console.log);
-		}
 
-		console.log(selection.toString());
+	const iframe = document.getElementById('anki-generator-frame');
+
+	if (selection && selection.toString() && !iframe) {
+		const rect = selection.getRangeAt(0).getBoundingClientRect();
+		const newIframe = createIframe(rect.top, rect.left);
+
+		document.body.appendChild(newIframe);
 	}
 }));
 
+document.addEventListener('mousedown', () => {
+	const iframe = document.getElementById('anki-generator-frame');
+
+	if (iframe) document.body.removeChild(iframe);
+});
--- js/popup.js
+++ js/popup.js
@@ -1,34 +1,5 @@
 import { checkAnkiConnectStatus } from './api.js';
 
-function createPopup() {
-	const url = chrome.runtime.getURL('./open-card-generator.html');
-
-	/** @type {chrome.windows.CreateData} */
-	const popup = {
-		height: 100,
-		width: 100,
-		top: 0,
-		left: 0,
-		state: 'normal',
-		type: 'popup',
-		url
-	}
-
-	return new Promise((resolve, reject) => {
-		chrome.windows.create(
-			popup,
-			(result) => {
-				const error = chrome.runtime.lastError;
-				if (error) {
-					reject(new Error(error.message));
-				} else {
-					resolve(/** @type {chrome.windows.Window} */(result));
-				}
-			}
-		);
-	});
-}
-
 /**
  * @param {boolean} ankiConnected
  */
